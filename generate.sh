fd8c126c28ae9881cd881c425c067b865a64365d
02lyx <yuanxinliu2029@u.northwestern.edu>
2024-11-21 18:46:05 -0600

    env_setup

--- generate.sh
+++ generate.sh
@@ -2,18 +2,18 @@
 # The generated samples will be stored at output_dir + local_index + ".jsonl
 
 my_world_size=4 # how many gpu you use
-infer_model="pipeline_test/baseline_sft_iter2"
-prompt_dir="RLHF4MATH/prompt_iter3"
-# mkdir data
-output_dir=./data/basesft_iter3
+infer_model="ZhangShenao/baseline-gemma-2-2b-it-sft"
+prompt_dir=Yuanxin-Liu/Test-Dataset
+mkdir data
+output_dir=./data/basesft_iter
 
-# conda init
-# conda activate vllm
+conda init
+conda activate data-eval
 
-CUDA_VISIBLE_DEVICES=1 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 16 --temperature 1.0 --local_index 0 --my_world_size ${my_world_size}  &
-CUDA_VISIBLE_DEVICES=2 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 16 --temperature 1.0 --local_index 1 --my_world_size ${my_world_size}  &
-CUDA_VISIBLE_DEVICES=3 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 16 --temperature 1.0 --local_index 2 --my_world_size ${my_world_size}  &
-CUDA_VISIBLE_DEVICES=4 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 16 --temperature 1.0 --local_index 3 --my_world_size ${my_world_size}  &
+CUDA_VISIBLE_DEVICES=0 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 16 --temperature 1.0 --local_index 0 --my_world_size ${my_world_size}  &
+CUDA_VISIBLE_DEVICES=1 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 16 --temperature 1.0 --local_index 1 --my_world_size ${my_world_size}  &
+CUDA_VISIBLE_DEVICES=2 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 16 --temperature 1.0 --local_index 2 --my_world_size ${my_world_size}  &
+CUDA_VISIBLE_DEVICES=3 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 16 --temperature 1.0 --local_index 3 --my_world_size ${my_world_size}  &
 # CUDA_VISIBLE_DEVICES=4 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 4 --temperature 1.0 --local_index 4 --my_world_size ${my_world_size}  &
 # CUDA_VISIBLE_DEVICES=5 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 4 --temperature 1.0 --local_index 5 --my_world_size ${my_world_size}  &
 # CUDA_VISIBLE_DEVICES=6 python ./generation/gen_hf2.py --model_name_or_path ${infer_model} --dataset_name_or_path ${prompt_dir} --output_dir ${output_dir} --K 4 --temperature 1.0 --local_index 6 --my_world_size ${my_world_size}  &
@@ -21,5 +21,5 @@ CUDA_VISIBLE_DEVICES=4 python ./generation/gen_hf2.py --model_name_or_path ${inf
 
 # then, we merge the 8 datasets into one dataset.
 wait
-# python ./generation/merge_data.py --base_path ${output_dir} --output_dir './data/gen_data.json' --num_datasets ${my_world_size}
+python ./generation/merge_data.py --base_path ${output_dir} --output_dir './data/gen_data.json' --num_datasets ${my_world_size}
 # python ./generation/merge_data.py --base_path './data/basesft_iter3' --output_dir './data/basesft_iter3/merge_data.json' --num_datasets 4
---
+++ readme.md
@@ -0,0 +1,30 @@
+## Step 1: Set up environment 
+We set up two environments, ```data-eval``` is used for generating data, annotating data, evaluating model perforcement, and the other ```train-only``` is used for DPO training.
+To install ```data-eval```, please do the following
+
+```sh
+conda env create -f data-eval.yaml
+```
+And to install training environment, do the following:
+```sh
+conda create -n train-only python=3.10.9
+conda activate train-only
+
+git clone https://github.com/huggingface/alignment-handbook.git
+cd ./alignment-handbook/
+git checkout 27f7dbf00663dab66ad7334afb7a1311fa251f41
+pip3 install torch==2.1.2 torchvision torchaudio
+python -m pip install .
+pip install flash-attn==2.6.3
+pip install accelerate==0.34.0
+pip install huggingface-hub==0.24.7
+pip install transformers==4.45.0
+pip install trl==0.11.1
+pip install wandb==0.17.7
+pip install numpy==1.26.4
+wandb login
+huggingface-cli login
+cd ..
+```
+## Step 2: Run the main loop
+Run the bash to train it iteratively 
